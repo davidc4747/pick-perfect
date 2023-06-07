1ad920a643bae9b41c6202ba6a29d54b1a02ac22
David Chung <davidc4747@yahoo.com>
2023-06-07 12:33:54 -0700

    Style Settings Page

--- src/renderer/pages/settings/settings.tsx
+++ src/renderer/pages/settings/settings.tsx
@@ -1,23 +1,57 @@
 import { Link } from "react-router-dom";
-import { home } from "./settings.module.css";
+import { settings, header, buttonGroup } from "./settings.module.css";
 
 /* ===================== *\
     # Settings
 \* ===================== */
 
 interface PropTypes {
-    value?: string;
+    onSave(): void;
 }
 
+type Settings = {
+    accecptReadyCheck: boolean;
+    requeue: boolean;
+    setSmite: "false" | "D" | "F";
+};
+
 export default function Settings(props: PropTypes): React.ReactElement {
-    const { value } = props;
+    const { onSave } = props;
 
     return (
-        <>
-            <Link className={home} to="/">
-                Home
-            </Link>
-            <h1>Settings Page!</h1>
-        </>
+        <section className={settings}>
+            <h1 className={header}>Settings</h1>
+            <label>
+                <span>Auto Import Smite (when Jungling) </span>
+                <select id="smite" className="select" value="D">
+                    <option value="false">No Key</option>
+                    <option value="D">D</option>
+                    <option value="F">F</option>
+                </select>
+            </label>
+
+            <label>
+                <span>Accept Ready Check</span>
+                <input className="chk" type="checkbox" checked />
+            </label>
+
+            <label>
+                <span>ReQueue after Honor</span>
+                <input className="chk" type="checkbox" checked />
+            </label>
+
+            <div className={buttonGroup}>
+                <Link className="btn" to="/">
+                    Cancel
+                </Link>
+
+                <button
+                    className={["btn", "btn--primary"].join(" ")}
+                    onClick={onSave}
+                >
+                    Save
+                </button>
+            </div>
+        </section>
     );
 }
